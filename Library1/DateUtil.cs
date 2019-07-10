4c3d545ed1aa3e3d4ad043d7bf7d5441bfb4d74b
vladyslav.hrona <vladyslav.hrona@acropolium.com>
2019-07-10 19:03:52 +0300

    add ParseToLocalTime method

--- Library1/DateUtil.cs
+++ Library1/DateUtil.cs
@@ -7,5 +7,15 @@ namespace Library1
     public class DateUtil
     {
         public static DateTime GetCurrent() => DateTime.Now;
+
+        public static DateTime? ParseToLocalTime(string data)
+        {
+            if (DateTime.TryParse(data, out var dt))
+            {
+                return dt.ToLocalTime();
+            };
+
+            return null;
+        }
     }
 }
