b7680374509ab996c304c034a10494f57159be43
vladyslav.hrona <vladyslav.hrona@acropolium.com>
2019-07-10 17:26:05 +0300

    add project files

---
+++ Library1/DateUtil.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library1
+{
+    public class DateUtil
+    {
+        public static DateTime GetCurrent() => DateTime.Now;
+    }
+}
